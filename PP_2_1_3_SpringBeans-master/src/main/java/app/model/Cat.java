d0da8a966dfdd7e6b004b9486a4db0c806ecca53
stkllo0 <106972900+stkllo0@users.noreply.github.com>
2023-01-28 20:23:19 +0300

    Update Cat.java

--- PP_2_1_3_SpringBeans-master/src/main/java/app/model/Cat.java
+++ PP_2_1_3_SpringBeans-master/src/main/java/app/model/Cat.java
@@ -4,6 +4,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Cat extends Animal {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     @Override
     public String toString() {
         return "Im a Cat";
